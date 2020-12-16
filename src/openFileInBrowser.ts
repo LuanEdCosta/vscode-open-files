0d9dc327cffcaa00150c1528f61b6d8c0e033522
Luan Eduardo da Costa <luan1346@gmail.com>
2020-12-16 17:54:16 -0300

    First version of the open file in browser feature

--- src/openFileInBrowser.ts
+++ src/openFileInBrowser.ts
@@ -1,12 +1,34 @@
 import * as vscode from 'vscode'
 
+type Params = {
+  fsPath: string
+  path: string
+  scheme: string
+}
+
 export class OpenFileInBrowser {
   constructor(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand(
       'vscode-open-files.openFileInBrowser',
-      async () => {},
+      openFileInBrowser,
     )
 
     context.subscriptions.push(disposable)
   }
+}
+
+async function openFileInBrowser(params?: Params) {
+  if (params) {
+    await vscode.env.openExternal(vscode.Uri.parse(`file://${params.path}`))
+    return
+  }
+
+  if (!vscode.window.activeTextEditor) {
+    return vscode.window.showInformationMessage(
+      'You need to open a file and focus it in the VsCode to execute this command',
+    )
+  }
+
+  const openedFileUri = vscode.window.activeTextEditor.document.uri
+  await vscode.env.openExternal(openedFileUri)
 }
