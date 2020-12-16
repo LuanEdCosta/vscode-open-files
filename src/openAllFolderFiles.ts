9119aa79d148dd4057c753bf7676792377d60c3b
Luan Eduardo da Costa <luan1346@gmail.com>
2020-12-16 16:42:06 -0300

    First version of the open all folder files feature

--- src/openAllFolderFiles.ts
+++ src/openAllFolderFiles.ts
@@ -4,26 +4,62 @@ export class OpenAllFolderFiles {
   constructor(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand(
       'vscode-open-files.openAllFolderFiles',
-      async () => {
-        const [rootPathUri] = vscode.workspace.workspaceFolders
-          ? vscode.workspace.workspaceFolders
-          : []
-
-        const workspacePath = rootPathUri.uri.fsPath
-
-        const uri = vscode.Uri.file(`${workspacePath}/javascript.js`)
-
-        vscode.workspace.openTextDocument(uri).then(
-          (document: vscode.TextDocument) => {
-            vscode.window.showTextDocument(document, 1, true)
-          },
-          (error) => {
-            vscode.window.showErrorMessage(error.message)
-          },
-        )
-      },
+      openAllFolderFiles,
     )
 
     context.subscriptions.push(disposable)
+  }
+}
+
+async function openAllFolderFiles() {
+  if (!vscode.workspace.workspaceFolders) {
+    return vscode.window.showInformationMessage('No folder or workspace opened')
+  }
+
+  const search = await vscode.window.showInputBox({
+    placeHolder: 'Folder path (allow nested folders). Ex: src/components',
+    prompt: 'To open all files in the root directory type: **',
+    validateInput(value) {
+      const firstChar = value.trim().charAt(0)
+
+      if (firstChar === '.' || firstChar === '/' || firstChar === '\\') {
+        return 'Do not put slash or a dot at the beginning of the path'
+      }
+
+      return null
+    },
+  })
+
+  const trimmedSearch = search?.trim()
+
+  if (trimmedSearch) {
+    const lastChar = trimmedSearch.charAt(trimmedSearch.length - 1)
+    const hasSlashAtTheEnd = lastChar === '/' || lastChar === '\\'
+
+    const uriArray = await vscode.workspace.findFiles(
+      `${trimmedSearch}${hasSlashAtTheEnd ? '**' : '/**'}`,
+      '**​/node_modules/**',
+    )
+
+    if (!uriArray.length) {
+      return vscode.window.showInformationMessage(
+        `No files found inside the folder "${trimmedSearch}"`,
+      )
+    }
+
+    uriArray.forEach((uri) => {
+      vscode.workspace.openTextDocument(uri).then(
+        (document: vscode.TextDocument) => {
+          vscode.window.showTextDocument(document, {
+            preview: false,
+            preserveFocus: true,
+            viewColumn: 1,
+          })
+        },
+        (error) => {
+          vscode.window.showErrorMessage(error.message)
+        },
+      )
+    })
   }
 }
