efdf3094839cb9698acd0cfcba042499fbbd19b9
Luan Eduardo da Costa <luan1346@gmail.com>
2021-08-22 19:15:57 -0300

    Add missing test files

---
+++ src/__tests__/openFileInBrowser.test.ts
@@ -0,0 +1,5 @@
+describe('Open file in browser', () => {
+  it('should open the file in the browser', () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ src/__tests__/openRelatedFiles.test.ts
@@ -0,0 +1,5 @@
+describe('Open all related files', () => {
+  it('should open all related files', () => {
+    expect(true).toBe(true)
+  })
+})
